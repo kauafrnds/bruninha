ef04f43ede79cda9b7459b38bb0706a15598fbea
kauafrnds <147107762+kauafrnds@users.noreply.github.com>
2023-10-05 21:03:15 -0300

    subi os arquivos do meu site

---
+++ dados.php
@@ -0,0 +1,47 @@
+<!DOCTYPE hmtl>
+<html lang="en">
+<head>
+    <title>Bruna</title>
+</head>
+<body>
+    <body bgcolor="purple">
+    <center>
+        <h2> Parabéns Loira do Tchan, você passou no teste!! </h2>
+        <img src= "bruna.jpeg"width="150" height="150">
+        <br>
+<h1>Seja bem-vinda ao seu Little Museu</h1>
+<p><h3>O time do coração da Bruna é o <font color="bf0000"> Flamengo</h3></font></p>
+<img src= "flamengo.jpeg"width="300" height="300">
+<img src="gabigol.jpeg" height="300" width="300">
+<br>
+<p><h3>A banda favorita da Bruna é <font color="yellow">One Direction</h3></font></p>
+<img src= "od1.jpeg"width="500" height="300">
+<br>
+<p><h3>A personagem favorita da Bruna é a <font color="f256b6">Rapunzel</h3></font></p>
+<img src= "rapunzel.jpeg"width="300" height="300">
+<img src= "rapunzel1.jpeg"width="300" height="300">
+<br>
+<p><h3>A Bruna ama ir para a <font color="afe865">Praia</h3></font></p>
+<img src= "praia.jpeg"width="300" height="300">
+<img src= "praia1.jpeg"width="300" height="300">
+<p><h3>A fruta favorita da Bruna é <font color="f74f4f">Melancia</h3></font></p>
+<img src= "melancia.jpeg"width="300" height="300">
+<img src= "melancia1.jpeg"width="300" height="300">
+<br>
+
+<p><h2>Para fechar da melhor forma, aqui vai as fotos que você mais gosta 🤍</p></h2>
+<img src= "zap.jpeg"width="200" height="300">
+<img src= "zap1.jpeg"width="200" height="300">
+<img src= "zap2.jpeg"width="200" height="300">
+<br>
+<img src= "zap3.jpeg"width="200" height="300">
+<img src= "zap4.jpeg"width="200" height="300">
+<img src= "zap5.jpeg"width="200" height="300">
+<br>
+
+<h3> Agora para o que você está fazendo e clica nesse link </h3>
+   <h4><a href="https://youtu.be/VRpzJabYlQQ?si=fjWIFyoYXgCVXt-p"><font color="black">Clique aqui e seja feliz</font></a></h4>
+   <h5>(Kauãzin aq gosta mt de você)</h5>
+   <img src= "matue1.jpeg"width="300" height="300">
+</body>
+</html>
